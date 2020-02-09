380fe7fd2b069741a33468da7118e3dc055f051e
dillontryhorn <dillontryhorn@gmail.com>
2020-02-09 15:22:41 -0500

    Finished task 1. Game updates at approximately 60Hz with some margin of error

--- game_loop/main.cpp
+++ game_loop/main.cpp
@@ -12,7 +12,6 @@ int main() {
       auto game = std::make_unique<Game>("1st Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
    
       std::cout << "Starting game loop" << std::endl;
-      auto startLoop = std::chrono::system_clock::now();
       while (game->running()) {
          auto start = std::chrono::system_clock::now();
          game->handle_events();
@@ -22,9 +21,6 @@ int main() {
          auto duration = end - start;
          std::this_thread::sleep_for(std::chrono::duration<double>(1.0/60.0) - duration); //60 updates per second, 60Hz
       }
-      auto endLoop = std::chrono::system_clock::now();
-      auto durationLoop = endLoop - startLoop;
-      std::cout << durationLoop.count() << std::endl;
    } catch(std::runtime_error &e) {
       std::cout << e.what() << std::endl; //Catch any runtime errors, especially from constructor
    }
