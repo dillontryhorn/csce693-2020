0f7f9d8583ccfeff1b6d1b9fb511287de3627cea
dillontryhorn <dillontryhorn@gmail.com>
2020-02-09 15:14:10 -0500

    Added timer for total

--- game_loop/main.cpp
+++ game_loop/main.cpp
@@ -12,6 +12,7 @@ int main() {
       auto game = std::make_unique<Game>("1st Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
    
       std::cout << "Starting game loop" << std::endl;
+      auto startLoop = std::chrono::system_clock::now();
       while (game->running()) {
          auto start = std::chrono::system_clock::now();
          game->handle_events();
@@ -21,6 +22,9 @@ int main() {
          auto duration = end - start;
          std::this_thread::sleep_for(std::chrono::milliseconds(16) - duration); //60 updates per second, 60Hz
       }
+      auto endLoop = std::chrono::system_clock::now();
+      auto durationLoop = endLoop - startLoop;
+      std::cout << durationLoop << std::endl;
    } catch(std::runtime_error &e) {
       std::cout << e.what() << std::endl; //Catch any runtime errors, especially from constructor
    }
