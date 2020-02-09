8436e7f124f36be86a25db8ae2ecc29a71090a4d
dillontryhorn <dillontryhorn@gmail.com>
2020-02-09 15:01:47 -0500

    Fixed error

--- game_loop/main.cpp
+++ game_loop/main.cpp
@@ -20,11 +20,11 @@ int main() {
          std::this_thread::sleep_for(std::chrono::duration<float>(1.0f/60.0f)); //60 updates per second, 60Hz
       }
       auto stop = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+      std::cout << duration.count() << std::endl;
    } catch(std::runtime_error &e) {
       std::cout << e.what() << std::endl; //Catch any runtime errors, especially from constructor
    }
-   auto duration = duration_cast<microseconds>(stop - start);
-   std::cout << duration << std::endl;
    return 0;
 }
 
